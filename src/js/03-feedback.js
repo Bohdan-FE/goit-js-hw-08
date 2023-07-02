4c8293ee26122cd3f7e7ee876d35079e43124423
Bohdan <112878985+Bohdan-FE@users.noreply.github.com>
2023-07-02 21:56:44 +0200

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,14 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const data = {
-    email: '',
-    message: '',
-}
+  email: '',
+  message: '',
+};
 if (localStorage.getItem('feedback-form-state')) {
-    data.email = JSON.parse(
-        localStorage.getItem('feedback-form-state')
-    ).email;
-    data.message = JSON.parse(
-        localStorage.getItem('feedback-form-state')
-    ).message;
+  data.email = JSON.parse(localStorage.getItem('feedback-form-state')).email;
+  data.message = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  ).message;
 };
 form.addEventListener('input', throttle(inputHandler, 500));
 form.addEventListener('submit', submitHandler);
@@ -24,24 +22,16 @@ if (JSON.parse(localStorage.getItem('feedback-form-state'))) {
 };
 function inputHandler(e) {
   if (e.target.getAttribute('name') === 'email') {
-      data.email = e.target.value;
+    data.email = e.target.value;
   } else if (e.target.getAttribute('name') === 'message') {
-      data.message = e.target.value;
-  }
-    localStorage.setItem('feedback-form-state', JSON.stringify(data));
+    data.message = e.target.value;
+    };
+  localStorage.setItem('feedback-form-state', JSON.stringify(data));
 };
-function submitHandler(e) { 
-    e.preventDefault();
-    console.log(data);  
-    if (document.querySelector('input').value) {
-        document.querySelector('input').value = '';
-    } else if (document.querySelector('textarea').value) {
-        document.querySelector('textarea').value = '';
-    }
-    if (document.querySelector('input').value || document.querySelector('textarea').value) {
-        localStorage.removeItem('feedback-form-state')
-        document.querySelector('input').value = '';
-        document.querySelector('textarea').value = '';  
-    }
-       
-}
+function submitHandler(e) {
+  e.preventDefault();
+  console.log(data);
+  localStorage.removeItem('feedback-form-state');
+  document.querySelector('input').value = '';
+  document.querySelector('textarea').value = '';
+};
